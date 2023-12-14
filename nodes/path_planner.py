88aed1b19878d76d73180a6a4ac710d934edb3c7
Thies Lennart Alff <lennart.alff@gmail.com>
2023-12-14 20:11:20 +0000

    code cleanup

--- nodes/path_planner.py
+++ nodes/path_planner.py
@@ -261,7 +261,97 @@ class PathPlanner(Node):
             return True
         return False
 
-    def on_viewpoints(self, msg: Viewpoints):  # noqa: C901
+    def handle_mission_completed(self):
+        self.get_logger().info('Mission completed.')
+        if not self.do_stop():
+            self.get_logger().error(
+                'All waypoints completed, but could not '
+                'stop the path_follower. Trying again...',
+                throttle_duration_sec=1.0)
+            return
+        self.state = State.IDLE
+
+    def compute_new_path(self, viewpoints: Viewpoints):
+        i = self.find_first_uncompleted_viewpoint(viewpoints)
+        # start position and is treated differently.
+        if i < 1:
+            return
+        # complete them all.
+        # We do nothing smart here. We keep the order in which we received
+        # the waypoints and connect them by straight lines.
+        # No collision avoidance.
+        # We only perform a collision detection and give up in case that
+        # our path of straight lines collides with anything.
+        # Not very clever, eh?
+
+        # now get the remaining uncompleted viewpoints. In general, we can
+        # assume that all the following viewpoints are uncompleted, since
+        # we complete them in the same order as we get them in the
+        # viewpoints message. But it is not that hard to double-check it.
+        viewpoint_poses = [
+            v.pose for v in viewpoints.viewpoints[i:] if not v.completed
+        ]
+        # get the most recently visited viewpoint. Since we do not change
+        # the order of the viewpoints, this will be the viewpoint right
+        # before the first uncompleted viewpoint in the list, i.e. i-1
+        p0 = viewpoints.viewpoints[i - 1].pose
+        viewpoint_poses.insert(0, p0)
+
+        # now we can finally call our super smart function to compute
+        # the path piecewise between the viewpoints
+        return [
+            self.compute_path_segment(viewpoint_poses[i - 1],
+                                      viewpoint_poses[i])
+            for i in range(1, len(viewpoint_poses))
+        ]
+
+    def handle_no_collision_free_path(self):
+        self.get_logger().fatal('We have a collision in our current segment!'
+                                'Giving up...')
+        if self.do_stop():
+            self.state = State.IDLE
+        else:
+            self.state = State.UNSET
+
+    def do_normal_operation(self, viewpoints: Viewpoints):
+        # what we need to do:
+        # - check if the viewpoints changed, if so, recalculate the path
+        i = self.find_first_uncompleted_viewpoint(viewpoints)
+        # we completed our mission!
+        if i < 0:
+            self.handle_mission_completed()
+            return
+
+        if (not self.recomputation_required
+            ) or self.target_viewpoint_index == i:
+            # we are still chasing the same viewpoint. Nothing to do.
+            return
+        self.get_logger().info('Computing new path segments')
+        self.target_viewpoint_index = i
+        if i == 0:
+            p = viewpoints.viewpoints[0].pose
+            if not self.move_to_start(p, p):
+                self.get_logger().fatal(
+                    'Could not move to first viewpoint. Giving up...')
+                if self.do_stop():
+                    self.state = State.IDLE
+                else:
+                    self.state = State.UNSET
+            return
+
+        path_segments = self.compute_new_path(viewpoints)
+        if not path_segments:
+            self.get_logger().error(
+                'This is a logic error. The cases that would have lead to '
+                'no valid path_segments should have been handled before')
+            return
+        if path_segments[0]['collision_indices']:
+            self.handle_no_collision_free_path()
+            return
+        self.set_new_path(path_segments[0]['path'])
+        return
+
+    def on_viewpoints(self, msg: Viewpoints):
         if self.state == State.IDLE:
             return
         if self.state == State.UNSET:
@@ -275,126 +365,7 @@ class PathPlanner(Node):
             # corresponding service was called
             return
         if self.state == State.NORMAL_OPERATION:
-            # what we need to do:
-            # - check if the viewpoints changed, if so, recalculate the path
-            i = self.find_first_uncompleted_viewpoint(msg)
-            # we completed our mission!
-            if i < 0:
-                self.get_logger().info('Mission completed.')
-                if not self.do_stop():
-                    self.get_logger().error(
-                        'All waypoints completed, but could not '
-                        'stop the path_follower. Trying again...',
-                        throttle_duration_sec=1.0)
-                    return
-                self.state = State.IDLE
-                return
-
-            if (not self.recomputation_required
-                ) or self.target_viewpoint_index == i:
-                # we are still chasing the same viewpoint. Nothing to do.
-                return
-            self.get_logger().info('Computing new path segments')
-            self.target_viewpoint_index = i
-            if i == 0:
-                p = msg.viewpoints[0].pose
-                if not self.move_to_start(p, p):
-                    self.get_logger().fatal(
-                        'Could not move to first viewpoint. Giving up...')
-                    if self.do_stop():
-                        self.state = State.IDLE
-                    else:
-                        self.state = State.UNSET
-                return
-            # start position and is treated differently. we could bring them
-            # in an appropriate order or do whatever with them. The task is to
-            # complete them all.
-            # We do nothing smart here. We keep the order in which we received
-            # them and connect them by straight lines. No collision avoidance.
-            # We only perform a collision detection and give up in case that
-            # our path of straight lines collides with anything.
-            # Not very clever, eh?
-
-            # now get the remaining uncompleted viewpoints. In general, we can
-            # assume that all the following viewpoints are uncompleted, since
-            # we complete them in the same order as we get them in the
-            # viewpoints message. But it is not that hard to double-check it.
-            viewpoint_poses = [
-                v.pose for v in msg.viewpoints[i:] if not v.completed
-            ]
-            # get the most recently visited viewpoint. Since we do not change
-            # the order of the viewpoints, this will be the viewpoint right
-            # before the first uncompleted viewpoint in the list, i.e. i-1
-            p0 = msg.viewpoints[i - 1].pose
-            viewpoint_poses.insert(0, p0)
-
-            # now we can finally call our super smart function to compute
-            # the path piecewise between the viewpoints
-            path_segments = [
-                self.compute_path_segment(viewpoint_poses[i - 1],
-                                          viewpoint_poses[i])
-                for i in range(1, len(viewpoint_poses))
-            ]
-            if path_segments[0]['collision_indices']:
-                self.get_logger().fatal(
-                    'We have a collision in our current segment!'
-                    'Giving up...')
-                if self.do_stop():
-                    self.state = State.IDLE
-                else:
-                    self.state = State.UNSET
-                return
-            self.set_new_path(path_segments[0]['path'])
-            return
-
-    def on_viewpoints_old(self, msg: Viewpoints):
-        if not self.occupancy_grid:
-            return
-        start_viewpoint = msg.viewpoints[0]
-        if not start_viewpoint.completed and not self.remaining_segments:
-            self.remaining_segments = [
-                self.create_initial_segment(start_viewpoint),
-            ]
-            path = self.create_path()
-            if not self.set_new_path(path):
-                self.get_logger().error('Failed to set new path')
-            return
-        current_segment = self.remaining_segments[0]
-        self.viewpoints = msg.viewpoints
-        i = self.find_first_uncompleted_viewpoint(msg)
-        if i < 0:
-            self.get_logger().warn(
-                'If we are not done, something unexpected happend!', once=True)
-            return
-        if i < 1:
-            return
-        self.waypoints = []
-        self.orientations = []
-        self.store_viewpoint(msg.viewpoints[i - 1], self.waypoints,
-                             self.orientations)
-
-        self.store_viewpoints(msg.viewpoints[i:])
-        self.update_segments()
-        if not self.remaining_segments:
-            return
-        if self.remaining_segments[0].points == current_segment.points:
-            return
-        path = self.create_path()
-        if not path:
-            return
-        self.set_new_path(path)
-
-    def store_viewpoints(self, viewpoints):
-        for viewpoint in viewpoints:
-            if viewpoint.completed:
-                continue
-            self.store_viewpoint(viewpoint, self.waypoints, self.orientations)
-
-    def store_viewpoint(self, viewpoint, waypoints, orientations):
-        p = viewpoint.pose.position
-        waypoints.append([p.x, p.y, p.z])
-        q = viewpoint.pose.orientation
-        orientations.append(q)
+            self.do_normal_operation(msg)
 
     def find_first_uncompleted_viewpoint(self, viewpoints: Viewpoints):
         for i, viewpoint in enumerate(viewpoints.viewpoints):
@@ -402,47 +373,6 @@ class PathPlanner(Node):
                 return i
         # This should not happen!
         return -1
-
-    def create_path(self):
-        if not self.remaining_segments:
-            self.get_logger().warn('No remaining segments.')
-            return None
-        segment: PathSegment = self.remaining_segments[0]
-        world_points = segment.world_points(self.occupancy_grid.info.resolution)
-        z = self.waypoints[1][2]
-        q = self.orientations[1]
-        path = Path()
-        stamp = self.get_clock().now().to_msg()
-        for p_xy in world_points:
-            pose = PoseStamped()
-            pose.header.frame_id = 'map'
-            pose.header.stamp = stamp
-            pose.pose.position.x = p_xy[0]
-            pose.pose.position.y = p_xy[1]
-            pose.pose.position.z = z
-            pose.pose.orientation = q
-            path.poses.append(pose)
-        path.header.frame_id = 'map'
-        path.header.stamp = stamp
-        return path
-
-    def update_segments(self):
-        segments = self.compute_path_segments()
-        if not segments:
-            self.get_logger().warn(
-                'Could not create any path segment. '
-                'Maybe no waypoints have been received yet.',
-                throttle_duration_sec=5.0)
-            return
-        collision_indices = self.compute_path_collisions(segments)
-        collision_detected = bool(collision_indices)
-
-        if not collision_detected:
-            self.get_logger().info('Path is collision free :-)')
-            self.remaining_segments = segments
-        else:
-            self.get_logger().info(
-                f'Collisions in segments: {collision_indices}')
 
     def on_occupancy_grid(self, msg: OccupancyGrid):
         self.occupancy_grid = msg
@@ -477,82 +407,12 @@ class PathPlanner(Node):
         self.set_new_path_future = self.set_path_client.call_async(request)
         return True
 
-    def segments_to_world_points(self, segments: list[PathSegment]):
-        world_points = []
-        for _i, segment in enumerate(segments):
-            world_points.extend(
-                segment.world_points(self.occupancy_grid.info.resolution))
-        return world_points
-
     def publish_path_marker(self, segments: list[PathSegment]):
         msg = self.path_marker
         world_points = self.segments_to_world_points(segments)
         msg.points = [Point(x=p[0], y=p[1], z=-0.5) for p in world_points]
         msg.header.stamp = self.get_clock().now().to_msg()
         self.path_marker_pub.publish(msg)
-
-    def get_line_points(self, x0, y0, x1, y1):
-        dx = abs(x1 - x0)
-        sx = 1 if x0 < x1 else -1
-        dy = -abs(y1 - y0)
-        sy = 1 if y0 < y1 else -1
-        error = dx + dy
-
-        x = x0
-        y = y0
-        points = []
-        while True:
-            points.append([int(x), int(y)])
-            if x == x1 and y == y1:
-                break
-            doubled_error = 2 * error
-            if doubled_error >= dy:
-                if x == x1:
-                    break
-                error += dy
-                x += sx
-            if doubled_error <= dx:
-                if y == y1:
-                    break
-                error += dx
-                y += +sy
-        return points
-
-    def detect_collision(self, grid_points):
-        for pixel in grid_points:
-            if self.occupancy_matrix[pixel[1], pixel[0]] >= 50:
-                return True
-        return False
-
-    def compute_path_segments(self):
-        n = len(self.waypoints)
-        if n < 2:
-            return []
-        p0_world = self.waypoints[0]
-        prev_point = world_to_matrix(p0_world[0], p0_world[1],
-                                     self.occupancy_grid.info.resolution)
-        self.get_logger().info(f'Start grid: {prev_point}')
-        segments = []
-        for i in range(1, n):
-            self.get_logger().info(f'Computing path segment {i}/{n-1}')
-            point = self.waypoints[i]
-            x, y = world_to_matrix(point[0], point[1],
-                                   self.occupancy_grid.info.resolution)
-            grid_points = self.get_line_points(prev_point[0], prev_point[1], x,
-                                               y)
-            segment = PathSegment()
-            segment.points = grid_points
-            segments.append(segment)
-            prev_point = [x, y]
-        return segments
-
-    def compute_path_collisions(self, segments):
-        collision_indices = []
-        segment: PathSegment
-        for i, segment in enumerate(segments):
-            if segment.detect_collision(self.occupancy_matrix):
-                collision_indices.append(i)
-        return collision_indices
 
 
 def main():
