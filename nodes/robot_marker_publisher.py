906a1a5d725109ee6254c5f8f5b5a85a3e9ba05d
Thies Lennart Alff <lennart.alff@gmail.com>
2023-12-18 10:53:30 +0000

    publish pool mesh marker

--- nodes/robot_marker_publisher.py
+++ nodes/robot_marker_publisher.py
@@ -14,11 +14,13 @@ class MapperNode(Node):
                                                 topic='robot_marker',
                                                 qos_profile=1)
 
-        self.obstacle_sub = self.create_subscription(
+        self.pose_sub = self.create_subscription(
             msg_type=PoseWithCovarianceStamped,
             topic='vision_pose_cov',
             callback=self.on_pose,
             qos_profile=1)
+
+        self.timer = self.create_timer(1.0, self.on_timer)
 
     def on_pose(self, msg: PoseWithCovarianceStamped):
         marker = Marker()
@@ -35,6 +37,27 @@ class MapperNode(Node):
         marker.scale.y = 1.0
         marker.scale.z = 1.0
         mesh_resource = 'package://hippo_sim/models/bluerov/meshes/bluerov.dae'
+        marker.mesh_resource = mesh_resource
+
+        self.marker_pub.publish(marker)
+
+    def on_timer(self):
+        marker = Marker()
+        marker.header.stamp = self.get_clock().now().to_msg()
+        marker.header.frame_id = 'map'
+        marker.type = Marker.MESH_RESOURCE
+        marker.ns = 'pool'
+        marker.pose.position.x = 1.0
+        marker.pose.position.y = 2.0
+        marker.pose.position.z = -1.5
+        marker.color.r = 0.435
+        marker.color.g = 0.725
+        marker.color.b = 1.0
+        marker.color.a = 1.0
+        marker.scale.x = 1.0
+        marker.scale.y = 1.0
+        marker.scale.z = 1.0
+        mesh_resource = 'package://hippo_sim/models/pool/meshes/pool.dae'
         marker.mesh_resource = mesh_resource
 
         self.marker_pub.publish(marker)
