0abc16f01c62109acf42d348280c3c1e5e1560f3
NBauschmann <nathalie_bauschmann@hotmail.de>
2023-12-19 14:59:57 +0100

    rename launch file, use sim time

--- launch/final_project.launch.py
+++ launch/final_project.launch.py
@@ -14,20 +14,17 @@ def generate_launch_description() -> LaunchDescription:
     arg = DeclareLaunchArgument('vehicle_name')
     launch_description.add_action(arg)
 
+    arg = DeclareLaunchArgument('use_sim_time')
+    launch_description.add_action(arg)
+
     package_path = get_package_share_path('final_project')
     mapping_params_file_path = str(package_path / 'config/mapping_params.yaml')
-    str(package_path / 'test.urdf')
 
     scenario_arg = DeclareLaunchArgument(
         name='scenario',
         default_value=str(1),
         description='The number of the scenario')
     launch_description.add_action(scenario_arg)
-    viewpoints_arg = DeclareLaunchArgument(
-        name='viewpoints',
-        default_value=str(0),
-        description='The number of the viewpoints file')
-    launch_description.add_action(viewpoints_arg)
 
     group = GroupAction([
         PushROSNamespace(LaunchConfiguration('vehicle_name')),
@@ -36,7 +33,10 @@ def generate_launch_description() -> LaunchDescription:
             package='final_project',
             parameters=[
                 LaunchConfiguration('mapping_params',
-                                    default=mapping_params_file_path)
+                                    default=mapping_params_file_path),
+                {
+                    'use_sim_time': LaunchConfiguration('use_sim_time'),
+                },
             ],
         ),
         Node(
@@ -45,30 +45,50 @@ def generate_launch_description() -> LaunchDescription:
             parameters=[
                 {
                     'scenario': LaunchConfiguration('scenario'),
-                    'viewpoints': LaunchConfiguration('viewpoints'),
+                    'use_sim_time': LaunchConfiguration('use_sim_time'),
                 },
             ],
         ),
         Node(
             executable='path_planner.py',
             package='final_project',
-            # arguments=['--ros-args', '--log-level', 'warn'],
+            parameters=[
+                {
+                    'use_sim_time': LaunchConfiguration('use_sim_time'),
+                },
+            ],
         ),
         Node(
             executable='path_follower.py',
             package='final_project',
+            parameters=[
+                {
+                    'use_sim_time': LaunchConfiguration('use_sim_time'),
+                },
+            ],
         ),
-        Node(
-            executable='position_controller.py',
-            package='final_project',
-        ),
+        Node(executable='position_controller.py',
+             package='final_project',
+             parameters=[{
+                 'use_sim_time': LaunchConfiguration('use_sim_time'),
+             }]),
         Node(
             executable='yaw_controller.py',
             package='final_project',
+            parameters=[
+                {
+                    'use_sim_time': LaunchConfiguration('use_sim_time'),
+                },
+            ],
         ),
         Node(
             executable='robot_marker_publisher.py',
             package='final_project',
+            parameters=[
+                {
+                    'use_sim_time': LaunchConfiguration('use_sim_time'),
+                },
+            ],
         ),
     ])
     launch_description.add_action(group)
